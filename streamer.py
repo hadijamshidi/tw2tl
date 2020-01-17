1d521279dfb47a3fd11fb0cc418553e9ca5d6f26
hadijamshidi <m.hadi.jamshidi@gmail.com>
2020-01-16 17:03:58 -0800

    forward telegram

--- streamer.py
+++ streamer.py
@@ -4,12 +4,12 @@ import settings
 import time
 from datetime import datetime
 
-auth = tweepy.OAuthHandler(settings.API_KEY, settings.API_KEY_SECRET)
-auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
+# auth = tweepy.OAuthHandler(settings.API_KEY, settings.API_KEY_SECRET)
+# auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
 
-api = tweepy.API(auth)
+# api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
+# public_tweets = api.home_timeline()
 
 
 # for tweet in public_tweets:
@@ -50,27 +50,61 @@ class MyStreamListener(tweepy.StreamListener):
         return datetime.now()
 
 
-following_user_ids = settings.TWITTER_USER_IDS
-while True:
-    myStreamListener = MyStreamListener()
-    try:
-        myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-        myStream.filter(follow=following_user_ids)
-        # myStream.filter(follow=following_user_ids, is_async=True)
-    except Exception as e:
-        print("Exception", myStreamListener.now())
-        print(e)
-        time.sleep(2)
+# following_user_ids = settings.TWITTER_USER_IDS
+# while True:
+#     myStreamListener = MyStreamListener()
+#     try:
+#         myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+#         myStream.filter(follow=following_user_ids)
+#         # myStream.filter(follow=following_user_ids, is_async=True)
+#     except Exception as e:
+#         print("Exception", myStreamListener.now())
+#         print(e)
+#         time.sleep(2)
 
-
+import telethon.sync
 from telethon import TelegramClient, events
-
+from telethon.sync import TelegramClient
+from telethon import functions, types
+from telethon.tl.types import UpdateNewChannelMessage
 client = TelegramClient('hadi', settings.TELEGRAM_API_ID, settings.TELEGRAM_API_ID_HASH)
 
 @client.on(events.NewMessage)
 async def my_event_handler(event):
-    if 'hello' in event.raw_text:
-        await event.reply('hi!')
+    # print("EVENT:", event)
+    # print("TYPE:", type(event.original_update), str())
+    # print("TO_ID:", event.original_update.message.to_id, event.original_update.message.to_id.channel_id)
+    update = event.original_update
+    # # if isinstance(original_update, UpdateNewChannelMessage):
+    # with TelegramClient('hadi', settings.TELEGRAM_API_ID, settings.TELEGRAM_API_ID_HASH) as client:
+    #     result = client(functions.channels.GetFullChannelRequest(
+    #         channel=event.original_update.message.to_id.channel_id
+    #     ))
+    #     print(result.stringify())
+    try:
+        if isinstance(update, UpdateNewChannelMessage):
+            # print("FROM A CHANNEL")
+            chan = await client.get_entity(event.original_update.message.to_id)
+            print("USERNAME:", chan.username)
+            if chan.username in settings.TELEGRAM_FAVORITE_CHANNELS:
+                print("FORWARDING MESSAGE!")
+                await client.forward_messages(settings.TELEGRAM_CHANNEL, event.original_update.message)
+                pass
+            # Finally, I need this ...
+            # print(chan.title)
+            # result = await client(functions.channels.GetFullChannelRequest(
+            #     channel=event.original_update.message.to_id.channel_id
+            # ))
+            # print("result", chan)
+            # print("CHANNEL:", chan.__dict__)
+            # print("USERNAME:", chan.username)
+    except Exception as e:
+        print(e)
+    # else:
+    #     print("NOT CHANNEL")
+    # print(event.__dict__)
+    # if 'hello' in event.raw_text:
+        # await event.reply('hi!')
 
 client.start()
 client.run_until_disconnected()
